8d02ea915f8bda9d30b6e6632de730c27543bb9e
Lucas Bertoldo <lucas.fber@gmail.com>
2019-09-18 22:51:31 -0300

    1001 - Extremely Basic, 1002 - Area of A Circle -- C

---
+++ beginner/1002-area-of-a-circle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+  const double PI = 3.14159;
+  double raio, area;
+
+  scanf("%lf", &raio);
+
+  area = PI * (raio * raio);
+
+  printf("A=%.4lf\n", area);
+  return 0;
+}
